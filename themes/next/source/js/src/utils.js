6651d16ef282aba2b1652a8fad0a0906950976c1
Yangkai.Shen <237497819@qq.com>
2019-03-12 15:07:14 +0800

    update theme

--- themes/next/source/js/src/utils.js
+++ themes/next/source/js/src/utils.js
@@ -11,6 +11,7 @@ NexT.utils = NexT.$u = {
       .not('.group-picture img, .post-gallery img')
       .each(function() {
         var $image = $(this);
+        if ($(this).hasClass('nofancybox')) return;
         var imageTitle = $image.attr('title');
         var $imageWrapLink = $image.parent('a');
 
